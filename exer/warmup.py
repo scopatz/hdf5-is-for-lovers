f6619aa30744b3c3f3e1ae383ed531b825107a7f
Illia Polosukhin <ilblackdragon@gmail.com>
2013-03-18 09:45:51 -0700

    Used with when open hdf5 file, to handle close correcly in cases when opening\creating\appending\closing fails

--- exer/warmup.py
+++ exer/warmup.py
@@ -1,8 +1,7 @@
 import numpy as np
 import tables as tb
 
-f = tb.openFile('temp.h5', 'a')
-heart = np.ones(42, dtype=[('rate', int), ('beat', float)])
-f.createTable('/', 'heart', heart)
-f.close()
+with tb.openFile('temp.h5', 'a') as f:
+    heart = np.ones(42, dtype=[('rate', int), ('beat', float)])
+    f.createTable('/', 'heart', heart)
 
